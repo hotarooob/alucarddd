49eec1160244913e8a27b2a80b6a698c3d2b09cd
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-03 08:35:20 +0300

    Update question.js

--- plugins/question.js
+++ plugins/question.js
@@ -1,27 +1,29 @@
 import fs from 'fs';
-const timeout = 60000;
-const poin = 500;
+const timeout =  ❪ 60000 ❫;
+const poin =  ❪ 500 ❫;
 const handler = async (m, {conn, usedPrefix}) => {
   conn.tekateki = conn.tekateki ? conn.tekateki : {};
   const id = m.chat;
   if (id in conn.tekateki) {
-    conn.reply(m.chat, 'لا تزال هناك سؤال لم تتم الإجابة عليها في هذه الدردشة', conn.tekateki[id][0]);
+    conn.reply(m.chat, '〈 لا تزال هناك سؤال لم تتم الإجابة عليها في هذه الدردشة 💧↫〉', conn.tekateki[id][0]);
     throw false;
   }
   const tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`));
   const json = tekateki[Math.floor(Math.random() * tekateki.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
-  const caption = `
+  const caption =`
+ *┏━━━━━━⊰⚔️⊱━━━━━━┓*
 ⷮ *${json.question}*
-*• وقت:* ${(timeout / 1000).toFixed(2)} ثواني
-*• الجوائز:* +${poin} Exp
-`.trim();
+*❍⤪┇⌚ الــوقــت ${(timeout / 1000).toFixed(2)} ثــانـيـة
+*❍⤪┇⚡️ الجوائز:* +${poin}  نــقـطـه 
+*┗━━━━━━⊰⚔️⊱━━━━━━┛*
+*┇𝑬𝑹𝑰𝑵- 𝑩𝑶𝑻┇*`.trim();
   conn.tekateki[id] = [
     await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `انتهى الوقت!\n*إجابة:* ${json.response}`, conn.tekateki[id][0]);
+      if (conn.tekateki[id]) await conn.reply(m.chat, `*『𒀭خلص الوقت┇𒀭』*\n💧↫الاجـابـة┇⤪❍* ${json.response}`, conn.tekateki[id][0]);
       delete conn.tekateki[id];
     }, timeout)];
 };
