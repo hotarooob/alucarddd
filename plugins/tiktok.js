3e52fdec9df3d9a1eeab4599f2fca706ab714bde
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-03 23:44:09 +0300

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,30 +3,27 @@ import fg from 'api-dylux'
 import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!args[0]) throw ` هات رابط الفيديو \n  هكذا ⇲ : \n${usedPrefix + command} https://vm.tiktok.com/ZMjDJRRUS/`
-if (!args[0].match(/tiktok/gi)) throw `لنك بايظ يهطف  💀`
+if (!args[0]) throw ` أين هو رابط فيديو التكتوك الذي تود تحميله\n\n 📌 مثال : \n${usedPrefix + command} https://vm.tiktok.com/ZMNqyusVD/?k=1`
+if (!args[0].match(/tiktok/gi)) throw `❎ verify that the link is from tiktok`
 
 try {
     let p = await fg.tiktok(args[0]) 
     let te = `
- *❄️┇يجيب لك فيد من التيك┋❄️* 
-*༺═━═⊱╊⊰ ⏣ ⊱╉⊰═━═༻*
-♤ *الاسم⚡️:* ${p.unique_id}
-♤ *العنوان🪄:* ${p.title}
-♤ *الوقت⌚:* ${p.duration}
-
-*『𝑬𝑹𝑰𝑵- 𝑩𝑶𝑻┇✜』*
-`
+┌─⊷ TIKTOK
+▢ *Username:* ${p.unique_id}
+▢ *Description:* ${p.title}
+▢ *Duration:* ${p.duration}
+└───────────`
    conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
     } catch {  	
 	const { author: { nickname }, video, description } = await tiktokdl(args[0])
          .catch(async _ => await tiktokdlv2(args[0]))
          .catch(async _ => await tiktokdlv3(args[0]))
     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw 'فيه غلط في التحميل '
+    if (!url) throw '❎ خطأ في تحميل الفيديو '
      conn.sendFile(m.chat, url, 'fb.mp4', `
-┌─⊷ * 🤖 تيك توك 🤖*
-▢ *الاسم📮 :* ${nickname} ${description ? `\n▢ *الوصف 👀:* ${description}` : ''}
+┌─⊷ *TIKTOK DL*
+▢ *Username:* ${nickname} ${description ? `\n▢ *Description:* ${description}` : ''}
 └───────────`, m)
 } 
     
