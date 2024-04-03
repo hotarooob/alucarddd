1914bacea4a3d8be58a9c099ba2fb63b68688a69
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-03 06:44:50 +0300

    Update هل.js

--- plugins/هل.js
+++ plugins/هل.js
@@ -2,7 +2,7 @@ import util from 'util'
 import path from 'path'
 let user = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) throw `*أدخــل الـسـؤال !*`
+if (!text) throw `*〄┇أدخــل الـسـؤال┇〄*`
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
 let x = `${pickRandom(['احــتـمـال قـلـيـل' ,'نــعم بـالـتـأكـيد', 'لا أعـتـقـد', 'مــستـحـيــل'])}`
