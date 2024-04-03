36ff8d0cb71c9705781d985d2642e584584e2cbb
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-03 09:12:24 +0300

    Add files via upload

---
+++ plugins/تصميم.js
@@ -0,0 +1,13 @@
+import  fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+  let response = args.join(' ').split('|')
+  let res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&script=fluffy-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
+  if (!args[0]) throw 'نسيت النص الذي تبي تصممه                  *✦━━━━━━ ♤ ━━━━━━━✦*'
+  m.reply('_يرجى الانتظار..._')
+  conn.sendFile(m.chat, res, 'Noureddine.jpg', `مفيش شكرا كده ولا حاجه💫 
+  *✦━━━━━━ ♤ ━━━━━━━✦*`, m, false)
+}
+handler.help = ['flaming1'].map(v => v + ' <text>')
+handler.tags = ['maker','logo']
+handler.command = /^(تصميم)$/i
+export default handler
