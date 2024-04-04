55650fa801f4870331e78a8e552b06ad059f0d12
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-04 10:43:54 +0300

    Update انمي.js

--- plugins/انمي.js
+++ plugins/انمي.js
@@ -2,12 +2,12 @@ import translate from '@vitalets/google-translate-api'
 import { Anime } from "@shineiichijo/marika"
 const client = new Anime();
 let handler = async(m, { conn, text, usedPrefix }) => {
-if (!text) return m.reply(`*[🙄] *monster اكتب اسم الانمي الي تبغاه مثال انمي* `)
+if (!text) return m.reply(`*✓┃🚫┃حط اسم الانمي ال انت عايز تدور عليه ✓*`)
 try {  
 let anime = await client.searchAnime(text)
 let result = anime.data[0];
-let resultes = await translate(`${result.background}`, { to: 'ar', autoCorrect: true })   
-let resultes2 = await translate(`${result.synopsis}`, { to: 'ar', autoCorrect: true })   
+let resultes = await translate(`${result.background}`, { to: 'es', autoCorrect: true })   
+let resultes2 = await translate(`${result.synopsis}`, { to: 'es', autoCorrect: true })   
 let AnimeInfo = `
 🎀 • *الاسم:* ${result.title}
 🎋 • *شكل:* ${result.type}
@@ -27,7 +27,7 @@ let AnimeInfo = `
 ❄ • *سيرة:* ${resultes2.text}`
 conn.sendFile(m.chat, result.images.jpg.image_url, 'error.jpg', AnimeInfo, m)
 } catch {
-throw `*[❗] خطأ، حاول مرة أخرى*`  
+throw `*❮❗️┊خطأ، حاول مرة أخرى❯*`  
 }}
 handler.command = /^(anime|انمي)$/i
 export default handler 
