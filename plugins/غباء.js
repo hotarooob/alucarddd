56cb6c71c9e1acdff0401c94e78a9cd9645af068
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-04 08:15:08 +0300

    Update غباء.js

--- plugins/غباء.js
+++ plugins/غباء.js
@@ -2,7 +2,8 @@ let handler = async (m, { conn, command, text }) => {
 let stupidity = `*🤡 نسبة غباء 🤡*
 *نسبة اغباء ${text} 🤡هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
 *ربنا يشفيك😂💙*
-*✦━━━━━━[ 𝑬𝑹𝑰𝑵 ]━━━━━━✦*
+*✦━━━✤━ –⊰ ❖ ⊱– ━✤━━━✦
+*
 `.trim()
 m.reply(stupidity, null, { mentions: conn.parseMention(stupidity) })}
 handler.help = ['stupidity']
