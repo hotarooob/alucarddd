05264383da9e425f5624331f7f1e572f14f02c59
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-04 10:16:14 +0300

    Update حظي.js

--- plugins/حظي.js
+++ plugins/حظي.js
@@ -1,9 +1,10 @@
 let handler = async (m, { conn, command, text }) => {
 let love = `
-*🧧نسبة حظك🧧 من 100* ${text}  هي *${Math.floor(Math.random() * 100)}%* *من 100%*
+*🧧نسبة حظك📮 من 100* ${text}  هي *${Math.floor(Math.random() * 100)}%* *من 100%*
 *متزعلشي🥲💔ع حظك تتعود/ي*
 
-*╝•══━━━•〘•🎊•〙•━━━══•╚*
+*✦━━━✤━ –⊰ ❖ ⊱– ━✤━━━✦
+*
 `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love']
