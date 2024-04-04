2b2b3b2f8eb2453417991fd51620b911ceba3c3f
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-04 11:05:02 +0300

    خلفية.js

--- plugins/JOHAN-خلفية.js
+++ plugins/JOHAN-خلفية.js
@@ -1,18 +1,9 @@
-import {wallpaper} from '@bochilteam/scraper';
-import _translate from "./_translate.js"
-const tradutor = _translate.plugins.downloader_wallpaper
-// Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-// Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-// To set the language, in the root of the project, modify the config.json file.
-
-
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `${tradutor.texto1} ${usedPrefix + command} Minecraft*`;
-  const res = await wallpaper(text);
-  const img = res[Math.floor(Math.random() * res.length)];
-  conn.sendFile(m.chat, img, 'error.jpg', `${tradutor.texto2} ${text}*`, m);
-};
-handler.help = ['', '2'].map((v) => 'wallpaper' + v + ' <query>');
-handler.tags = ['downloader'];
-handler.command = /^(خلفية?)$/i;
-export default handler;
+import axios from "axios"
+let handler = async (m, {command, conn, usedPrefix}) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/socona12/test/main/5lfya`)).data  
+let haha = await res[Math.floor(res.length * Math.random())]    
+conn.sendFile(m.chat, haha, 'error.jpg', ``, m)
+}
+handler.command = handler.help = ['خلفيه', 'خلفية']
+handler.tags = ['anime']
+export default handler
